25d0db263b903db5856f51b1d49893b9da4bd719
iKoDesu <Jose.DiazV@Outlook.com>
2024-03-09 18:31:32 -0300

    Nuevo script

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cours.ControlDeVeriones
+{
+    public class Example : MonoBehaviour
+    {
+        int id;
+
+        private void Start()
+        {
+            id = 1;
+        }
+    }
+}
